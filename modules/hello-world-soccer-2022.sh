ce663b986dcdf64b7d0ba267e91ba4a921b3ccf9
wilkie <wilkie05@gmail.com>
2023-01-06 14:45:30 -0500

    Adds more hour of code 2022 modules.

---
+++ modules/hello-world-soccer-2022.sh
@@ -0,0 +1,38 @@
+# HoC2022 Hello World Soccer
+
+NAME="Hello World: Soccer (Hour of Code 2022)"
+
+COURSE=hello-world-soccer-2022
+LESSON=1
+
+# Other URLs to crawl
+URLS="
+"
+
+# Files to copy over (that might not be crawled)
+STATIC="
+api/v1/animation-library/default-spritelab-metadata/production
+api/v1/animation-library/manifest/spritelab/en_us
+"
+
+# Files to then parse for links
+PARSE="
+api/v1/animation-library/default-spritelab-metadata/production
+api/v1/animation-library/manifest/spritelab/en_us
+"
+
+# Files to copy from curriculum.code.org
+CURRICULUM_STATIC="
+"
+
+# Files to get from the main site using signed cookies
+RESTRICTED="
+"
+
+# Whole directories to copy over
+PATHS="
+blockly/media/spritelab
+"
+
+VIDEOS="
+"
---
+++ modules/starwarsblocks_1.sh
@@ -0,0 +1,30 @@
+# Star Wars (JavaScript)
+
+NAME="Star Wars - Hour of Code - Blocks"
+
+COURSE=starwarsblocks
+LESSON=1
+
+# Other URLs to crawl
+URLS="
+"
+
+# Files to copy over (that might not be crawled)
+STATIC="
+"
+
+# Files to copy from curriculum.code.org
+CURRICULUM_STATIC="
+"
+
+# Files to get from the main site using signed cookies
+RESTRICTED="
+"
+
+# Whole directories to copy over
+PATHS="
+blockly/media/skins/hoc2015x
+"
+
+VIDEOS="
+"
